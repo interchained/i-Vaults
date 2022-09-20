346c97c6022c338025379e0d391789bb13fdebdb
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-20 18:41:01 -0400

    Delete IWRAP.sol

--- interfaces/IWRAP.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-interface IWRAP {
-    function deposit() external payable;
-    function withdraw(uint amount) external;
-    function transfer(address dst, uint amount) external returns (bool);
-}
