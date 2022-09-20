7218ed9cce2cdae63ca350774f12d6dc14885c1d
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-20 19:03:39 -0400

    Update INTERFACES.sol

--- interfaces/INTERFACES.sol
+++ interfaces/INTERFACES.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
-abstract contract Context {
+abstract contract _MSG {
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
