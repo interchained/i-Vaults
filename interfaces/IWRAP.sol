ac4ff9f3d5ba9cc5601237164c73a7c30b491cae
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-20 18:15:02 -0400

    Update IWRAP.sol

--- interfaces/IWRAP.sol
+++ interfaces/IWRAP.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-interface IDividendDistributor {
+interface IWRAP {
     function deposit() external payable;
     function withdraw(uint amount) external;
     function transfer(address dst, uint amount) external returns (bool);
