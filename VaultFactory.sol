58c1474204d7e5d0bcb7c0b79397dc74d39d1ccc
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-22 16:05:08 -0400

    Update VaultFactory.sol

--- VaultFactory.sol
+++ VaultFactory.sol
@@ -193,6 +193,16 @@ contract KEK_Vault_Factory is iAuth, IVAULT {
         IRECEIVE(payable(vaultMap[number])).tokenizeWETH();
     }
 
+    function setOperator(uint256 number,address payable _newOperator) public virtual authorized() returns(bool) {
+        bool success = false;
+        if(_msgSender() == _community) {
+            (success) = IRECEIVE(payable(vaultMap[number])).setCommunity(payable(_newOperator));
+        } else if(_msgSender() == _community) {
+            (success) = IRECEIVE(payable(vaultMap[number])).setDevelopment(payable(_newOperator));
+        }
+        return success;
+    }
+
     function batchVaultRange(address token, uint256 fromWallet, uint256 toWallet) public override authorized() {
         uint256 n = fromWallet;
         bool isTokenTx = safeAddr(token) != false;
