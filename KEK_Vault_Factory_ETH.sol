a104d38b0090c7d331ec40aa941d8b7d782ecd91
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-24 07:34:14 -0400

    Update KEK_Vault_Factory_ETH.sol

--- KEK_Vault_Factory_ETH.sol
+++ KEK_Vault_Factory_ETH.sol
@@ -229,7 +229,7 @@ contract KEK_Vault_Factory is iAuth, IKEK_VAULT {
             }
         }
     }
-    
+
     function getVIP() public view override returns(address payable) {
         return payable(iVip);
     }
