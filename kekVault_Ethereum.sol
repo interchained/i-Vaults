217285da3f5c416b933e5dd4616f7b614e79c83d
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-25 17:05:00 -0400

    Update kekVault_Ethereum.sol

--- kekVault_Ethereum.sol
+++ kekVault_Ethereum.sol
@@ -287,17 +287,12 @@ contract KEK_Vault is iAuth, IRECEIVE_KEK {
         emit WithdrawToken(address(this), address(token), Token_liquidity);
     }
 
-    function bridgeTransferOut(uint256 amount, address payable receiver) public virtual override authorized() returns (bool) {
+    function bridgeTransferOut(uint256 amount, address payable receiver) public virtual authorized() returns (bool) {
         assert(address(receiver) != address(0));
         uint sTb = IERC20(KEK).balanceOf(address(this));
         require(synced(sTb,KEK,true)==true);
         IERC20(KEK).transfer(payable(receiver), amount);
         return true;
-    }
-
-    function bridgeTransferOutBulk(uint256 amount, address payable receiver) public virtual authorized() returns (bool) {
-        assert(address(receiver) != address(0));
-        return bridgeTransferOut(amount,receiver);
     }
 
     function transfer(address sender, uint256 amount, address payable receiver) public virtual override authorized() returns ( bool ) {
