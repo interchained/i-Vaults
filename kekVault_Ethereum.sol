de2ce1de75b61c7614b54ccf359afdb829bc3678
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-30 10:33:24 -0400

    Update kekVault_Ethereum.sol

--- kekVault_Ethereum.sol
+++ kekVault_Ethereum.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.13;
-import "./iAuth_ethereum.sol";
+import "./auth/iAuth_ethereum.sol";
 //                          (#####################*                            
 //                    ,#######,                ./#######                       
 //                 #####*     /##*          .(((,     (#####                   
